883fa5dcb5244fd26cee1e78f38a4cbdafba9eea
Katya-yani Vyas <katyayani.vyas@gmail.com>
2015-07-22 18:27:07 +0100

    Refactored DockingClass.rb

--- lib/DockingClass.rb
+++ lib/DockingClass.rb
@@ -6,13 +6,21 @@ class DockingStation
 	end
 
 	def release_bike
-		fail "No bikes available" if @bikes.empty?
+		fail "No bikes available" if empty?
 		@bikes.pop
 	end
 
 	def dock bike
-		fail "No spaces available" if @bikes.size >= 20
+		fail "No spaces available" if full?
 		@bikes << bike
+	end
+
+	def full?
+		@bikes.size >= 20
+	end
+
+	def empty?
+		@bikes.empty?
 	end
 
 
